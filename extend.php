4613a732c06b4a372dd91b6984a8789d8c9f072a
Vojtěch Oram <vojtech@oram.cz>
2020-07-08 23:26:10 +0200

    Loading extension translations

--- extend.php
+++ extend.php
@@ -9,4 +9,22 @@
  * file that was distributed with this source code.
  */
 
-return new Flarum\Extend\LanguagePack;
+use Flarum\Extend;
+
+
+// get all locale files
+$path = __DIR__.'/locale/';
+$extends = [
+    new Flarum\Extend\LanguagePack
+];
+foreach (scandir($path) as $f) {
+    if ($f == '.' || $f == '..')
+        continue;
+    foreach (scandir($path.$f) as $ff) {
+        if ($ff == '.' || $ff == '..')
+            continue;
+        array_push($extends, new Extend\Locales($path.$f.'/'.$ff));
+    }
+}
+
+return $extends;
